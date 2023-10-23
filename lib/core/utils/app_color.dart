317db6c9f6129e317fb3361daf55120290465e03
Eiad Hany <eiadhany04@gmail.com>
2023-10-23 17:55:44 +0300

    create app colors

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class Colors{
+static Color primaryColor =  const Color(0xffC49E85);
+static Color mainTextColor = const Color(0xff6B4B3E);
+static Color lightGreyColor = const Color(0xffC4C4C4);
+static Color blackColor =const Color(0xff333333);
+
+static Color deepGreyColor = const Color(0xff6F6460);
+
+}
