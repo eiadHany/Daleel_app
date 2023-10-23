4726c2458a13401b3d9d1bf0b60359163d5d95e4
Eiad Hany <eiadhany04@gmail.com>
2023-10-23 15:31:00 +0300

    add assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+abstract class Assets{
+  static String onBoarding1 = 'assets/images/on boarding 1.png';
+static String onBoarding2 = 'assets/images/on boarding 2.png';
+static String onBoarding3 = 'assets/images/on boarding 3.png';
+static String forgetPassword = 'assets/images/forget password.png';
+static String success = 'assets/images/Success.png';
+static String vector1 = 'assets/images/Vector (1).svg';
+static String vector2 = 'assets/images/Vector.svg';
+}
